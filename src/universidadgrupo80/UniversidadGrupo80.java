5cec4b400e1a12522893302fe234a5ff9f3b2b21
orli1208 <116685619+orli1208@users.noreply.github.com>
2023-09-07 19:39:52 -0300

    Update UniversidadGrupo80.java

--- src/universidadgrupo80/UniversidadGrupo80.java
+++ src/universidadgrupo80/UniversidadGrupo80.java
@@ -16,6 +16,8 @@ public class UniversidadGrupo80 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Holaaaa");
     }
     
 }
