201075a9b4308c61316416a2e2ce77692c7c98e9
Moksys1 <ariel21roldan@gmail.com>
2023-09-07 19:59:56 -0300

    Alumno completo
    Cree la clase alumno en el paquete entidades y lo complete con los atributos vistos en la guia

--- src/universidadgrupo80/accesoADatos/AlumnoData.java
+++ src/universidadgrupo80/accesoADatos/AlumnoData.java
@@ -1,14 +1,73 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package universidadgrupo80.accesoADatos;
 
-/**
- *
- * @author PC
- */
+import java.time.LocalDate;
 public class AlumnoData {
-    
+    private int idAlumno;
+    private String apellido;
+    private String nombre;
+    private LocalDate fechaNac;
+    private boolean activo;
+
+    public AlumnoData() {
+    }
+
+    public AlumnoData(int idAlumno, String apellido, String nombre, LocalDate fechaNac, boolean activo) {
+        this.idAlumno = idAlumno;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.fechaNac = fechaNac;
+        this.activo = activo;
+    }
+
+    public AlumnoData(String apellido, String nombre, LocalDate fechaNac, boolean activo) {
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.fechaNac = fechaNac;
+        this.activo = activo;
+    }
+
+    public int getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(LocalDate fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    @Override
+    public String toString() {
+        return "AlumnoData{" + "idAlumno=" + idAlumno + ", apellido=" + apellido + ", nombre=" + nombre + '}';
+    } 
 }
