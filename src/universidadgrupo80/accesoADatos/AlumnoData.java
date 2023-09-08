27b622b62ae04214ee23f2c01d0e01980de6ac65
KevinSepulveda93 <141686118+KevinSepulveda93@users.noreply.github.com>
2023-09-08 20:55:30 -0300

    metodo buscar alumno

--- src/universidadgrupo80/accesoADatos/AlumnoData.java
+++ src/universidadgrupo80/accesoADatos/AlumnoData.java
@@ -5,7 +5,6 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
@@ -13,34 +12,62 @@ import org.mariadb.jdbc.Statement;
 import universidadgrupo80.entidades.Alumno;
 
 public class AlumnoData {
-    private Connection con =null;
-    
+
+    private Connection con = null;
+
     public AlumnoData() {
         con = Conexion.getConnection();
     }
-    
-    public void guardarAlumno(Alumno alumno){
+
+    public void guardarAlumno(Alumno alumno) {
         String sql = "INSERT INTO alumno (dni, apellido, nombre, fechaNac, estado)"
                 + "VALUES (?, ?, ?, ?, ?)";
-        
+
         try {
-            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS );
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             ps.setInt(1, alumno.getDni());
             ps.setString(2, alumno.getApellido());
             ps.setString(3, alumno.getNombre());
             ps.setDate(4, Date.valueOf(alumno.getFechaNac()));
             ps.setBoolean(5, alumno.isActivo());
             ps.executeUpdate();
-            
+
             ResultSet rs = ps.getGeneratedKeys();
             if (rs.next()) {
                 alumno.setIdAlumno(rs.getInt(1));
                 JOptionPane.showMessageDialog(null, "Alumno añadido con exito.");
             }
             ps.close();
-            
+
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla alumno");
+
+        }
+    }
+
+    public Alumno BucarAlumno(int id) {
+        Alumno alumno = null;
+        String sql = "SELECT dni,apellido,nombre,fechaNac FROM alumno WHERE idAlumno=? AND estado =1";
+        PreparedStatement ps = null;
+        try {
+            ps=con.prepareStatement(sql);
+            ps.setInt(1,id);
+            ResultSet rs=ps.executeQuery();
+               if (rs.next()) {
+                alumno=new Alumno();
+                alumno.setIdAlumno(rs.getInt("idAlumno"));
+                alumno.setDni(rs.getInt("dni"));
+                alumno.setApellido(rs.getString("apellido"));
+                alumno.setNombre(rs.getString("nombre"));
+                alumno.setFechaNac(rs.getDate("fechaNac").toLocalDate());
+                alumno.setActivo(true);
+                
+                
+                JOptionPane.showMessageDialog(null, "Alumno añadido con exito.");
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(AlumnoData.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
