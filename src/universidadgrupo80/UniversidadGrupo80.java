cb5a31252553e69a50bdc69998934d25800f3d76
Moksys1 <ariel21roldan@gmail.com>
2023-09-12 20:10:08 -0300

    Añadida la Clase MateriaData

--- src/universidadgrupo80/UniversidadGrupo80.java
+++ src/universidadgrupo80/UniversidadGrupo80.java
@@ -26,12 +26,22 @@ public class UniversidadGrupo80 {
         
         //Connection con = Conexion.getConnection();
         
-        Alumno juancito = new Alumno(43856345, "Suarez", "Valentina", LocalDate.of(2001, 06, 26), true);
-        AlumnoData alu = new AlumnoData();
-        alu.guardarAlumno(juancito);
+        //Alumno juancito = new Alumno(23454323, "Martinez", "Pedro", LocalDate.of(1992, 01, 07), true);
+       // AlumnoData alu = new AlumnoData();
+        //alu.guardarAlumno(juancito);
         //alu.modificarAlumno(juancito);
         //alu.BuscarAlumno(1);
         //alu.eliminarAlumno(1);
+//        Alumno alumnoEncontrado = alu.BuscarAlumnoPorDni(43856345);
+//        System.out.println(alumnoEncontrado);
+//        AlumnoData alu = new AlumnoData();
+//        for (Alumno alumno: alu.listarAlumnos()) {
+//            System.out.println("DNI: "+alumno.getDni());
+//            System.out.println("Apellido: "+alumno.getApellido());
+//            System.out.println("Nombre: "+alumno.getNombre());
+//            System.out.println("Fecha de Nacimiento: "+alumno.getFechaNac());
+//            System.out.println("");
+        }
     }
     
 }
