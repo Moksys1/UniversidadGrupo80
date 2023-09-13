a63a6340073750e8ca33916b3d264f083a4a24d7
orli1208 <116685619+orli1208@users.noreply.github.com>
2023-09-13 18:34:39 -0300

    Vista de Menu Principal

--- src/universidadgrupo80/vistas/menuPrincipal.java
+++ src/universidadgrupo80/vistas/menuPrincipal.java
@@ -27,17 +27,68 @@ public class menuPrincipal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jMenu4 = new javax.swing.JMenu();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenu7 = new javax.swing.JMenu();
+        jMenu2 = new javax.swing.JMenu();
+        jMenu8 = new javax.swing.JMenu();
+        jMenu3 = new javax.swing.JMenu();
+        jMenu9 = new javax.swing.JMenu();
+        jMenu10 = new javax.swing.JMenu();
+        jMenu5 = new javax.swing.JMenu();
+        jMenu11 = new javax.swing.JMenu();
+        jMenu6 = new javax.swing.JMenu();
+
+        jMenu4.setText("jMenu4");
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jMenu1.setText("Alumno");
+
+        jMenu7.setText("Formulario de Alumno");
+        jMenu1.add(jMenu7);
+
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Materia");
+
+        jMenu8.setText("Formulario de Materia");
+        jMenu2.add(jMenu8);
+
+        jMenuBar1.add(jMenu2);
+
+        jMenu3.setText("Administracion ");
+
+        jMenu9.setText("Manejo de Inscripciones");
+        jMenu3.add(jMenu9);
+
+        jMenu10.setText("Manipulacion de Notas");
+        jMenu3.add(jMenu10);
+
+        jMenuBar1.add(jMenu3);
+
+        jMenu5.setText("Consultas");
+
+        jMenu11.setText("Alumnos por Materia");
+        jMenu5.add(jMenu11);
+
+        jMenuBar1.add(jMenu5);
+
+        jMenu6.setText("Salir");
+        jMenuBar1.add(jMenu6);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 583, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 282, Short.MAX_VALUE)
         );
 
         pack();
@@ -79,5 +130,17 @@ public class menuPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu10;
+    private javax.swing.JMenu jMenu11;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu3;
+    private javax.swing.JMenu jMenu4;
+    private javax.swing.JMenu jMenu5;
+    private javax.swing.JMenu jMenu6;
+    private javax.swing.JMenu jMenu7;
+    private javax.swing.JMenu jMenu8;
+    private javax.swing.JMenu jMenu9;
+    private javax.swing.JMenuBar jMenuBar1;
     // End of variables declaration//GEN-END:variables
 }
